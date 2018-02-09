0e8a535dda172654943ee35a12d3cb6af7825b98
Kelly Smith <ksmith2@unl.edu>
2018-02-09 10:23:09 -0600

    includes scatterplot of length by scalelength, colored by lake

--- fish-analysis.r
+++ fish-analysis.r
@@ -5,3 +5,8 @@ fish_data <- read.csv("Gaeta_etal_CLC_data_1.csv", stringsAsFactors = FALSE)
 fish_data_cat <- fish_data %>% mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
   filter(scalelength >= 1)
 
+library(ggplot2)
+
+f <- ggplot(fish_data_cat, aes(x = length, y = scalelength)) +
+  geom_point(aes(color = lakeid))
+f
