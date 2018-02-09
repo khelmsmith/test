78c92bd1828c2baf6ffe5373e431a24121eef0e9
Kelly Smith <ksmith2@unl.edu>
2018-02-09 10:05:16 -0600

    changed name of data file and changed corresponding read.csv code

--- fish-analysis.r
+++ fish-analysis.r
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-fish_data <- read.csv("Gaeta_etal_CLC_data.csv", stringsAsFactors = FALSE)
+fish_data <- read.csv("Gaeta_etal_CLC_data_1.csv", stringsAsFactors = FALSE)
 
 fish_data_cat <- fish_data %>% mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
   filter(scalelength >= 1)
