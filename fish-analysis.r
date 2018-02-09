bdf8be0b8c7c040c4980c7e87a4a96e75266a73b
Kelly Smith <ksmith2@unl.edu>
2018-02-09 09:49:09 -0600

    more efficient way to do the same thing

--- fish-analysis.r
+++ fish-analysis.r
@@ -2,6 +2,6 @@ library(tidyverse)
 
 fish_data <- read.csv("Gaeta_etal_CLC_data.csv", stringsAsFactors = FALSE)
 
-fish_data %>% mutate(length_cat = ifelse(length > 300, "big", "small")) -> fish_data_cat
-
+fish_data_cat <- fish_data %>% mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
+  filter(scalelength >= 1)
 
